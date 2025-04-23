69feac4b075f9d7fba17ec161e0dec960bfd29a3
abubaker <msabubaker1@gmail.com>
2025-04-23 11:38:49 +0530

    auth and middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express=require("express")
-
 const app=express()
 
-app.use("/test",(req,res)=>{
+
+app.use('/',(req,res,next)=>{
+    let token=true
+    if(!token){
+        res.send("error")
+    }else{
+        next()
+    }   
+})
+
+app.get("/users",(req,res)=>{
+    console.log(req.query)
     res.send("Hello from the server")
+})
+app.get("/users/userdetail",(req,res)=>{
+    res.send("user details")
 })
 app.listen(3000,()=>{
     console.log("lisnening to the port 3000")
