f3ab63aa6670a75e1ead045f6fa7048e5099a234
abubaker <msabubaker1@gmail.com>
2025-04-24 07:32:13 +0530

    craeting database schema and models

--- src/app.js
+++ src/app.js
@@ -1,23 +1,33 @@
 const express=require("express")
 const app=express()
+const connectDB=require('./config/database')
+const User=require('./models/user')
 
+app.post('/signup', async (req,res)=>{
+    //creating instance of schema
+    const user =new User({
+        firstName:"Umar",
+        lastName:"M S",
+        age:"31",
+        email:"umar@gmail.com",
+        password:"umar123"
+    })
+    await user.save()
+    try{
+        res.send("signup success")
 
-app.use('/',(req,res,next)=>{
-    let token=true
-    if(!token){
-        res.send("error")
-    }else{
-        next()
-    }   
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+connectDB().then(()=>{
+    console.log("DB connected sucessfully");
+    app.listen(3000,()=>{
+        console.log("lisnening to the port 3000");
+    })
+})
+.catch((err)=>{
+    console.log(err)
 })
 
-app.get("/users",(req,res)=>{
-    console.log(req.query)
-    res.send("Hello from the server")
-})
-app.get("/users/userdetail",(req,res)=>{
-    res.send("user details")
-})
-app.listen(3000,()=>{
-    console.log("lisnening to the port 3000")
-})
