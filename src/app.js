b53536b4689a89ef8c05a3c0e9c378317cd8f000
abubaker <msabubaker1@gmail.com>
2025-04-26 07:09:00 +0530

    sanitizing the api data and validators

--- src/app.js
+++ src/app.js
@@ -5,6 +5,35 @@ const User=require('./models/user')
 
 app.use(express.json());
 
+//get user by email
+app.get('/user',async(req,res)=>{
+    const emailId=req.body.emailId
+    try{
+        const user=await User.find({emailId:emailId})
+        console.log(user)
+    }
+    catch(err){
+        console.log(err)    
+    }
+})
+
+// get all user details , to show in the feed
+app.get('/feed',async(req,res)=>{
+    
+    try{
+        const user= await User.find()
+        if(user){
+            res.json(user)
+        }
+        else{
+            res.status(404).send('User not found')
+        }
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
 app.post('/signup', async (req,res)=>{
     //creating instance of schema
 
@@ -17,6 +46,56 @@ app.post('/signup', async (req,res)=>{
     }
     catch(err){
         console.log(err)
+        res.send(err.message)
+    }
+})
+
+// update emailId
+app.put('/user/:userid',async (req,res)=>{
+    const userId=req.params?.userId
+    const data=req.body
+    console.log(data)
+ 
+    
+    
+    try{
+        const allowedUpdates=["password","about","photoUrl","skills","lastName"];
+        const isupdatedAllowed=Object.keys(data).every((k)=>{
+           return allowedUpdates.includes(k)
+        })
+
+         
+        console.log(isupdatedAllowed)
+        if(!isupdatedAllowed){
+            throw new Error("Update not allowed")
+        }
+        const result=await User.findOneAndUpdate(
+            userId,
+            data,
+            {
+                runValidators:true
+            }
+        )
+        res.send("successfully updated",result) 
+    }
+    catch(err){
+        console.log(err)
+        res.send(err.message)
+    }
+})
+
+//Delete user
+
+app.delete('/deleteuser',async (req,res)=>{
+    console.log(req.body)
+    const userId=req.body.userId
+    try{
+        const deleted=  await User.findByIdAndDelete(userId)
+        console.log(deleted)
+        res.send("deleted sucessfully")
+    }
+    catch(err){
+        res.send(err)
     }
 })
 connectDB().then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,68 @@
 const mongoose=require("mongoose")
-
+const validator=require("validator")
 const {Schema}=mongoose
 
 const userSchema=new Schema({
-    firstName: String,
+    firstName: {
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        minlength:3,
+        maxlength:50
+    },
     lastName: String,
-    age:Number,
-    emailId:String,
-    password:String,
-    gender:String
+    age:{
+        type:String,
+        min:15,
+      
+    },
+    emailId:{
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("EmailId is not correct")
+            }           
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Password is not strong")
+            }           
+        }
+    },
+
+    about:{
+        type:String,
+        default:"This is default value for about"
+    },
+    photoUrl:{
+        type:String,
+        default:"https://www.freepik.com/free-photos-vectors/default-user",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Photo URL is not correct")
+            }           
+        }
+    },
+    skills:{
+        type:[String]
+    },
+    gender:{
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender is not valid");
+            }
+        }
+    }
+},{
+    timestamps:true
 })
 
 const User=mongoose.model('User',userSchema)
